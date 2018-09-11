8b4fcf875f454febe69c0e326eb099f25379eb6c
pangjian <pangjian1010@gmail.com>
2018-09-11 15:48:43 +0800

    主题

---
+++ blog/themes/hexo-theme-material-x/scripts/lib/css_lsload.js
@@ -0,0 +1,39 @@
+const path_for = require('./path_for');
+const get_file_hex = require('./get_file_hex');
+const fs = require('fs');
+
+function cssHelper() {
+  let result = '';
+  let path = '';
+  let key = '';
+
+  for (let i = 0, len = arguments.length; i < len; i++) {
+    if (typeof arguments[i] === 'string') {
+      path = arguments[i];
+      key = path;
+    } else {
+      path = arguments[i].path;
+      key = arguments[i].key;
+    }
+
+    if (i) result += '\n';
+
+    if (Array.isArray(path)) {
+      result += jsHelper.apply(this, path);
+    } else {
+      if (path.indexOf('?') < 0 && path.substring(path.length - 4, path.length) !== '.css') path += '.css';
+      const localpath = path_for.call(this, path);
+      result += `<style id="${key}"></style><script>if(typeof window.lsLoadCSSMaxNums === "undefined")window.lsLoadCSSMaxNums = 0;` +
+        'window.lsLoadCSSMaxNums++;' +
+        `lsloader.load("${key }","${
+        require('../../../../node_modules/hexo/lib/plugins/helper/url_for').call(this, path)
+        }${fs.existsSync(localpath) ? `?${  get_file_hex(localpath)}`:'' }",function(){if(typeof window.lsLoadCSSNums === "undefined")window.lsLoadCSSNums = 0;` +
+        'window.lsLoadCSSNums++;' +
+        'if(window.lsLoadCSSNums == window.lsLoadCSSMaxNums)document.documentElement.style.display="";' +
+        '}, false)</script>';
+    }
+  }
+  return result;
+}
+
+module.exports = cssHelper;
---
+++ blog/themes/hexo-theme-material-x/scripts/lib/js_hex.js
@@ -0,0 +1,26 @@
+
+
+let path_for = require('./path_for');
+let get_file_hex = require('./get_file_hex');
+
+function jsHelper() {
+  let result = '';
+  let path = '';
+
+  for (let i = 0, len = arguments.length; i < len; i++) {
+    path = arguments[i];
+
+    if (i) result += '\n';
+
+    if (Array.isArray(path)) {
+      result += jsHelper.apply(this, path);
+    } else {
+      if (path.indexOf('?') < 0 && path.substring(path.length - 3, path.length) !== '.js') path += '.js';
+      result += `<script src="${  require("../../../../node_modules/hexo/lib/plugins/helper/url_for").call(this,path)  }?${  get_file_hex(path_for.call(this,path))  }"></script>`;
+    }
+  }
+
+  return result;
+}
+
+module.exports = jsHelper;
