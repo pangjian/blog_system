471685c9110b60fbe7b952e4fb57c6f32f7e908a
pangjian <pangjian1010@gmail.com>
2018-11-05 15:44:03 +0800

    update

--- backend/app/controller/ys.js
+++ backend/app/controller/ys.js
@@ -4,7 +4,16 @@ const Controller = require('egg').Controller;
 
 class YsController extends Controller {
   async getUrls() {
-    this.ctx.body = 'hi, egg';
+    const result = await this.ctx.curl('https://open.ys7.com/view/h5/077e9a83d7c441f08bae826cd3455e99');
+    const resulrArray = result.data.split('\n');
+    let urlStr = '';
+    for (let i = 0; i < resulrArray.length; i++) {
+      if (resulrArray[i].indexof('playUrl') > 0) {
+        urlStr = resulrArray[i];
+        break;
+      }
+    }
+    this.ctx.body = urlStr;
   }
 }
 
--- backend/app/router.js
+++ backend/app/router.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const basepath = '/api';
+
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
-  router.get('/ys/geturls', controller.ys.getUrls);
+  router.get(basepath + '/', controller.home.index);
+  router.get(basepath + '/ys/geturls', controller.ys.getUrls);
 };
